15318f7db74034e8affcb7ec444b223387a97161
Jon Simons <jon@jonsimons.org>
2015-06-04 20:03:55 -0700

    example.sh: fix example to actually work

--- example.sh
+++ example.sh
@@ -7,27 +7,37 @@ set -e
 bt_init
 
 example_function () {
-  timeout 1.0s cat /dev/zero > /dev/null || true
+  timeout 2.0s cat /dev/zero | gzip > /dev/null || true
 }
 export -f example_function
 
-bt_start "full example"
+bt_start 'full example'
 
-bt_start "serial cats"
-bash -c "bt_start 'cat1'; example_function; bt_end 'cat1'"
-bash -c "bt_start 'cat2'; example_function; bt_end 'cat2'"
-bt_end "serial cats"
+bt_start 'serial cats'
+bash -c 'bt_start "cat + gzip 1"; example_function; bt_end "cat + gzip 1"'
+bash -c 'bt_start "cat + gzip 2"; example_function; bt_end "cat + gzip 2"'
+bt_end 'serial cats'
 
-bt_start "sleep 0.25"
+bt_start 'sleep 0.5'
 sleep 0.5
-bt_end "sleep 0.25"
+bt_end 'sleep 0.5'
 
-bt_start "parallel cats"
-bash -c "bt_start 'cat3'; example_function; bt_end 'cat3'" &
-bash -c "bt_start 'cat4'; example_function; bt_end 'cat4'" &
-wait 2>/dev/null || true
-bt_end "parallel cats"
+bt_start 'parallel cats'
 
-bt_end "full example"
+bash -c 'bt_start "cat + gzip 3"; example_function; bt_end "cat + gzip 3"' &
+pcat_pids=$!
+
+bash -c 'bt_start "cat + gzip 4"; example_function; bt_end "cat + gzip 4"' &
+pcat_pids="$! $pcat_pids"
+
+wait $pcat_pids 2>/dev/null || true
+
+bt_end 'parallel cats'
+
+bt_start 'final sleep 0.5'
+sleep 0.5
+bt_end 'final sleep 0.5'
+
+bt_end 'full example'
 
 bt_cleanup
